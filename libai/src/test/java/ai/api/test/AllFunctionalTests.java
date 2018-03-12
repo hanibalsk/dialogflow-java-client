71f8699f031e9d3591fc99a0c118b90288cc1251
hanibalsk <mj.janci@gmail.com>
2018-03-12 15:07:51 +0100

    Update AllFunctionalTests.java

--- libai/src/test/java/ai/api/test/AllFunctionalTests.java
+++ libai/src/test/java/ai/api/test/AllFunctionalTests.java
@@ -22,7 +22,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({
-	ProtocolProdTest.class,
+	//ProtocolProdTest.class,
 /* Disabled due to ProtocolProdTest is enough for now
 	DefaultProtocolTest.class,
 	V20150204ProtocolTest.class
